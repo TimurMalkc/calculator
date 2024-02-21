366722b5c25c97901850ad7c9a1cc55baa188405
TimurMalkc <timurmlkc@gmail.com>
2024-02-21 20:26:57 +0300

    added gui

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import tkinter as tk
-
 import ctypes
 
-
+equation = ""
 def isNumber(s):
     try:
         float(s)
@@ -10,13 +9,17 @@ def isNumber(s):
     except ValueError:
         return False
 
-def toPostFix(equation):
+def toPostFix():
+    global equation
     postfixEquation = ""
     operatorStack = ["("]
     operators = ["+","-","/","*","(",")"]
     tempStr = ""
-    equation += ")"
 
+    if (equation == ""):
+        return
+
+    equation += ")"
     for i in equation:
         if isNumber(i) or i == ".":
             tempStr += i
@@ -53,11 +56,15 @@ def toPostFix(equation):
 
     return postfixEquation
 
-def evaluatePostfix(equation):
+def evaluatePostfix():
+    global equation
     stackNums = []
     myRes = 0
-    equation = equation.split()
 
+    if (equation == None):
+        return
+
+    equation = equation.split()
     for i in equation:
         if isNumber(i):
             stackNums.append(float(i))
@@ -78,26 +85,81 @@ def evaluatePostfix(equation):
             stackNums.append(myRes)
             myRes = 0
 
-    return float(stackNums[0])
+    if(float(stackNums[0]) == int(stackNums[0])):
+        return int(stackNums[0])
+    else:
+        return float(stackNums[0])
 
+def submit():
+    global equation
+    equation = entry.get()
+    equation = toPostFix()
+    delete()
+    entry.insert(0, evaluatePostfix())
 
-#mystr = input()
-#mystr = toPostFix(mystr)
-#print(evaluatePostfix(mystr))
+def delete():
+    entry.delete(0, tk.END)
+def backspace():
+    entry.delete(len(entry.get())-1 ,tk.END)
+
+def insertText(char):
+    entry.insert(len(entry.get()), char)
 
 window = tk.Tk()
 window.title("Calculator")
-
 window.geometry("600x800")
+window.config(background="#34ebb4")
 
-myText = tk.Text(window, height = 5, width = 52)
+frameEntry = tk.Frame(window, pady=5, background="#34ebb4")
+frameEntry.pack()
+frameButtons = tk.Frame(window,pady=5, background="#34ebb4")
+frameButtons.pack()
 
-myLabel = tk.Label(window, text="Calculator")
-myLabel.config(font =("Courier", 14))
+entry = tk.Entry(frameEntry, font=("Ariel",30))
+entry.grid(row = 0, column = 0, sticky = tk.W, pady = 2)
 
 
+button1 = tk.Button(frameButtons, text="1", command = lambda: insertText("1"), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+button1.grid(row = 0, column = 0, sticky = tk.W, pady = 2)
+button2 = tk.Button(frameButtons, text="2", command = lambda: insertText("2"), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+button2.grid(row = 0, column = 1, sticky = tk.W, pady = 2)
+button3 = tk.Button(frameButtons, text="3", command = lambda: insertText("3"), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+button3.grid(row = 0, column = 2, sticky = tk.W, pady = 2)
+button4 = tk.Button(frameButtons, text="4", command = lambda: insertText("4"), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+button4.grid(row = 1, column = 0, sticky = tk.W, pady = 2)
+button5 = tk.Button(frameButtons, text="5", command = lambda: insertText("5"), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+button5.grid(row = 1, column = 1, sticky = tk.W, pady = 2)
+button6 = tk.Button(frameButtons, text="6", command = lambda: insertText("6"), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+button6.grid(row = 1, column = 2, sticky = tk.W, pady = 2)
+button7 = tk.Button(frameButtons, text="7", command = lambda: insertText("7"), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+button7.grid(row = 2, column = 0, sticky = tk.W, pady = 2)
+button8 = tk.Button(frameButtons, text="8", command = lambda: insertText("8"), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+button8.grid(row = 2, column = 1, sticky = tk.W, pady = 2)
+button9 = tk.Button(frameButtons, text="9", command = lambda: insertText("9"), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+button9.grid(row = 2, column = 2, sticky = tk.W, pady = 2)
+button0 = tk.Button(frameButtons, text="0", command = lambda: insertText("0"), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+button0.grid(row = 3, column = 0, sticky = tk.W, pady = 2)
+buttonP1 = tk.Button(frameButtons, text="(", command = lambda: insertText("("), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+buttonP1.grid(row = 3, column = 1, sticky = tk.W, pady = 2)
+buttonP2 = tk.Button(frameButtons, text=")", command = lambda: insertText(")"), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+buttonP2.grid(row = 3, column = 2, sticky = tk.W, pady = 2)
+buttonDot = tk.Button(frameButtons, text=".", command = lambda: insertText("."), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+buttonDot.grid(row = 4, column = 0, sticky = tk.W, pady = 2)
+buttonBs = tk.Button(frameButtons, text="<-", command=backspace, font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+buttonBs.grid(row = 4, column = 1, sticky = tk.W, pady = 2)
+buttonCl = tk.Button(frameButtons, text="Cl", command=delete, font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+buttonCl.grid(row = 4, column = 2, sticky = tk.W, pady = 2)
+buttonPlus = tk.Button(frameButtons, text="+", command = lambda: insertText("+"), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+buttonPlus.grid(row = 0, column = 3, sticky = tk.W, pady = 2, padx=(40,0))
+buttonMin = tk.Button(frameButtons, text="-", command = lambda: insertText("-"), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+buttonMin.grid(row = 1, column = 3, sticky = tk.W, pady = 2, padx=(40,0))
+buttonMul = tk.Button(frameButtons, text="*", command = lambda: insertText("*"), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+buttonMul.grid(row = 2, column = 3, sticky = tk.W, pady = 2, padx=(40,0))
+buttonDiv = tk.Button(frameButtons, text="/", command = lambda: insertText("/"), font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+buttonDiv.grid(row = 3, column = 3, sticky = tk.W, pady = 2, padx=(40,0))
+buttonEq = tk.Button(frameButtons, text="=", command = submit, font=("Comic Sans", 30), fg="blue", bg="purple", activeforeground="blue", activebackground="purple", width=5)
+buttonEq.grid(row = 4, column = 3, sticky = tk.W, pady = 2, padx=(40,0))
 
-tk.Button(window, text="Calculate").grid(column=1, row=0)
 
 ctypes.windll.shcore.SetProcessDpiAwareness(True)
 
